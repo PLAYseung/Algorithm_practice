4d27fc77162efdddce539afb2ac9c5b51ab9c232
PLAYseung <yuga9812@gmail.com>
2022-09-28 21:03:23 +0900

    보석 쇼핑 코드
    정답률 50% 수정 필요

--- shoppingJewel/shoppingJewel.py
+++ shoppingJewel/shoppingJewel.py
@@ -1,9 +1,17 @@
 def solution(gems):
     answer = []
-    unq_gem = list(set(gems))
-    print(unq_gem)
+    unq_gem = len(list(set(gems)))
+    copy_gems = []
+    for idx in range(len(gems)):
+        if unq_gem != len(list(set(gems[idx:]))):
+            copy_gems = gems[idx-1:]
+            answer.append(idx-1)
+            break
+    for idx in range(len(copy_gems)):
+        if unq_gem != len(list(set(copy_gems[:len(copy_gems)-idx]))):
+            answer.append(len(gems) - idx+1)
+            return(answer)
 
-    return answer
 
 if __name__=='__main__':
-    print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
+    print(solution(	["AA", "AB", "AC", "AA", "AC"]))
