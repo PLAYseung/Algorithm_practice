0beb5042ace783fb8da29ac4b0ac5567962991c6
yuganom <108312553+PLAYseung@users.noreply.github.com>
2022-10-02 14:25:50 +0900

    [Java] 기지국 설치 문제 풀이
    - Solution.java
       - 풀이를 정의 하고 풀려고 했던 방식
       - 풀이 실패 궂이 이런 식으로 할 필요가 없었음
    - sndSolution.java
       - position값을 변동을 주면서 적용 범위만 으로 서치 하는 형태
       - 풀이를 정리하여 readme 작성

--- build_baseStation/src/Main.java
+++ build_baseStation/src/Main.java
@@ -3,13 +3,18 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<Integer> station = new ArrayList<Integer>();
-        station.add(4);
-        station.add(11);
-    
-        Solution sol = new Solution();
+        // ArrayList<Integer> station = new ArrayList<Integer>();
+        // station.add(4);
+        // station.add(11);
+        int[] station = new int[2];
+        station[0] = 4;
+        station[1] = 11;
 
-        sol.solution(11, station, 1);
+        // Solution sol = new Solution();
+        sndSolution sndsol = new sndSolution();
+
+        // System.out.println(sol.solution(11, station, 1));
+        System.out.println(sndsol.solution(11, station, 1));
 
     }
 }
--- build_baseStation/src/Solution.java
+++ build_baseStation/src/Solution.java
@@ -1,13 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution{
     
-    public int solution(int n, ArrayList<Integer> station, int w) {
+    public int solution(int n, int[] station, int w) {
         int answer = 3;
-        
-        
-        System.out.println(station.size());
 
+        int[] emptyList = new int[n];
+        Arrays.fill(emptyList,0);
+        
+        // System.out.println(emptyList);
+        
+        for (Integer item:station){
+            System.out.println(item);
+            // System.out.println((emptyList[item-1]));
+            emptyList[item-1]++;
+        }
+        
+        System.out.println(Arrays.toString(emptyList));
+        
         return answer;
     }
 }
