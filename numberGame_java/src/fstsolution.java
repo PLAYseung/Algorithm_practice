1ff6f3bed502a601e96d18ea4c1fb4f21d250158
PLAYseung <yuga9812@gmail.com>
2022-10-10 22:59:22 +0900

    [Java] 숫자 게임 문제 풀이 - 1.2
    - 전체 테스트 통과 가능한 코드
    - 코드 단순화 필요해보임

--- numberGame_java/src/fstsolution.java
+++ numberGame_java/src/fstsolution.java
@@ -1,6 +1,39 @@
+import java.util.Arrays;
+
 public class fstsolution {
     public int solution(int[] A, int[] B) {
-        int answer = 0;
-        return answer;
+
+        int amax = 0;
+        int amin = 1000000000;
+        int bmax = 0;
+        int bmin = 1000000000;
+        int cnt = 0;
+
+        for (int numA : A) amax = Math.max(numA, amax);
+        for (int numA : A) amin = Math.min(numA, amin);
+
+        for (int numB : B) bmax = Math.max(numB, bmax);
+        for (int numB : B) bmin = Math.min(numB, bmin);
+
+        if (bmin > amax) return B.length;
+        if (amin > bmax) return 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int i = 0;
+
+        for (int a : A) {
+            for (; i < B.length ; i++) {
+                if (B[i] > a) {
+                    System.out.println(B[i]);
+                    i++;
+                    cnt++;
+                    break;
+                }
+            }
+        }
+
+        return cnt;
     }
 }
